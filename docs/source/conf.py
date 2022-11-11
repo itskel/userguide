9792a9908256f5ec029d0c06550892941f51b05c
CoSoDocs <116654228+CoSoDocs@users.noreply.github.com>
2022-11-11 09:09:00 -0500

    CSS Overrides
    Functionality Added

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,20 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Enable CSS Overrides
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+  import sphinx_rtd_theme
+  html_theme = 'sphinx_rtd_theme'
+  html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+  html_style = 'css/custom.css'
+else:
+  html_context = { 
+    'css_files': [
+        'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
+        'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
+        '_static/css/custom.css',
+    ],  
+  }   
